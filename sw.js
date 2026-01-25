dfd6a10f6fb078e37d17684a5242c5e1e51419bb
PuddingChef <87723091+PuddingChef@users.noreply.github.com>
2026-01-25 17:34:51 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "basketburst-v4-hybrid";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./favicon.svg",
+  "./favicon.png",
+  "./icons/icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-180.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req))
+  );
+});
